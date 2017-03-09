87abf18b2886d30133ea6b9f78613e5790a269dd
mikemey <miguel.maestro@gmx.co.uk>
2017-03-09 13:38:57 +0000

    PE-2919: upgrade to Play 2.5.12

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -27,14 +27,14 @@ private object AppDependencies {
 
   val compile = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current % "provided",
-    "uk.gov.hmrc" %% "http-verbs" % "6.2.0" // Note: Only the HeaderCarrier is used from this library.
+    "uk.gov.hmrc" %% "http-verbs" % "6.3.0" // Note: Only the HeaderCarrier is used from this library.
   )
 
   val testScope: String = "test"
 
   val test = Seq(
     "org.scalatest" %% "scalatest" % "2.2.6" % testScope,
-    "uk.gov.hmrc" %% "hmrctest" % "2.0.0" % testScope
+    "uk.gov.hmrc" %% "hmrctest" % "2.3.0" % testScope
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
